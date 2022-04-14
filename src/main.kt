804b1c3ba64f2dbdf8e0aef1ac01f3876f3ee9f6
hilda1737 <hildamoraahh@gmail.com>
2022-04-14 09:28:30 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,62 @@
+fun main(){
+
+    var  v= currentacount(321, "Hilda", 2344)
+println(v)
+
+deposit(500)
+
+
+
+}
+  data class currentacount(var accountnumber:Int,var accounname:String,var Balance:Int){
+
+  }
+    fun deposit(amount:Int){
+        var balance =0
+        balance+=amount
+        println(balance)
+
+
+
+    fun withdrwa(amount: Double){
+         var amount = 0
+       amount -=balance
+        println(amount)
+    }
+
+    fun details(X: Int,Y: Int,Z: String){
+     var b = X + Y + Z.toInt()
+        println(b)
+    }
+}
+
+ class savingaccount( accounname: String,accountnumber: Int,var withdrwal:Int){
+
+ }
+
+
+
+
+
+
+data class  product(val name:String,val weight:Int,var Category:String){
+ fun product(X:String){
+     var a = "hygyne"
+     var b = "groceries"
+     var c = "other"
+     when(X is
+
+ }
+
+
+
+
+fun word(names:String):Int {
+    names.forEach { name ->
+        (name % 2)
+        println()
+    }
+
+    return c
+
+}
